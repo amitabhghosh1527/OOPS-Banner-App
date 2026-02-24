56e63ac8c9d14fdf9c60725c588bb267cb444ec0
amitabhghosh1527 <amitabhghosh1527@gmail.com>
2026-02-24 11:29:38 +0530

    Amitabh added UC2

---
+++ UC2_Print_Banner.java
@@ -0,0 +1,15 @@
+public class UC2_Print_Banner {
+public class uc2 {
+    public static void main(String[] args) {
+
+        System.out.println("   ***     ******   ******      ******");
+        System.out.println(" **   **   *     *** *     ***  ***     ");
+        System.out.println("**     **  *     *** *     ***  ***     ");
+        System.out.println("**     **  ******    ******       ***   ");
+        System.out.println("**     **  *         *              *** ");
+        System.out.println(" **   **   *         *         **   *** ");
+        System.out.println("   ***     *         *           ****   ");
+
+    }
+}    
+}
